5eb59df5f68bb948ef235c7055102c5541a4b522
Maximus Champion <maximuschampion2002@gmail.com>
2025-06-05 14:03:22 +0300

    Refactored blog models: added signals, media, likes, and profile, also update requirements.txt & add new endpoints

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
+import pathlib
+import uuid
+
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -19,6 +30,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField(Category)
     is_published = models.BooleanField(default=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="liked_posts", blank=True)
 
     def __str__(self):
         return self.title
@@ -34,3 +46,30 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by{self.user} on {self.post.title}"
 
+
+def image_path(instance, filename):
+    base_name = getattr(instance.user, "username", None) or instance.user.email
+    filename = f"{slugify(base_name)}-{uuid.uuid4()}{pathlib.Path(filename).suffix}"
+    return pathlib.Path("upload/profiles/") / filename
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to=image_path, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user}'s profile"
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+class PostMedia(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="media")
+    file = models.FileField(upload_to="post_media/")
+
+    def __str__(self):
+        return f"{self.post}'s media file"
